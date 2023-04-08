adc1650dd71cc8bbc629555757acc82aa6d1f84a
abicraf <70151728+abicraf@users.noreply.github.com>
2023-04-09 01:50:12 +0800

    Update blind_auction.py

--- Day9_Blind_auction/blind_auction.py
+++ Day9_Blind_auction/blind_auction.py
@@ -1,6 +1,33 @@
-#Nesting Dictionary in a Dictionary
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],"total_visits":12},
-    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttart"], "population": ["many", "just a few"]},
-}
-print(travel_log)
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+print(logo)
+blind_auction = {}
+repeat = True
+
+
+def find_the_highest_bid(blind_auction):
+    highest_bid = 0
+    for bidder in blind_auction:
+        bid = blind_auction[bidder]
+        if bid > highest_bid:
+            highest_bid = bid
+            winner = bidder
+    print(f"The winner is {winner} with a bid of {highest_bid}")
+
+
+while repeat:
+    name = input("What's your name? ")
+    bid = int(input("What's your bid? "))
+    blind_auction[name] = bid
+
+    other_bidders = input(
+        "Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if other_bidders == 'yes':
+        clear()
+    else:
+        repeat = False
+        find_the_highest_bid(blind_auction)
+#print(blind_auction)
+# max_bid = max(blind_auction.values())
+# winner = max(zip(blind_auction.values(), blind_auction.keys()))[1]
