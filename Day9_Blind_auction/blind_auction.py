4e031a4a6d2f78bc12fc3a73e294e4db7cc74d25
abicraf <70151728+abicraf@users.noreply.github.com>
2023-04-09 01:47:10 +0800

    Add files via upload
    Day9 blind_auction
    Use dictionary

---
+++ Day9_Blind_auction/blind_auction.py
@@ -0,0 +1,6 @@
+#Nesting Dictionary in a Dictionary
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],"total_visits":12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttart"], "population": ["many", "just a few"]},
+}
+print(travel_log)
