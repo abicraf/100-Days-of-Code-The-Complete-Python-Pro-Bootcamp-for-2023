9dcaf9444424cff3351de28f556df78fb51b15df
abicraf <70151728+abicraf@users.noreply.github.com>
2023-04-16 01:46:16 +0800

    Add files via upload
    day17
    learn how to define class.method and class.attribute

---
+++ Day17_quiz_game/data.py
@@ -0,0 +1,37 @@
+question_data = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The main character in the Half-Life franchise is named Morgan Freeman.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "Clefairy was intended to be Ash&#039;s starting Pok&eacute;mon in the pilot episode of the cartoon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "The names of Tom Nook&#039;s cousins in the Animal Crossing franchise are named &quot;Timmy&quot; and &quot;Jimmy&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In Heroes of the Storm, the Cursed Hollow map gimmick requires players to kill the undead to curse the enemy team.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy the supernatural area known as  &quot;the Zone&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Mathematics", "type": "boolean", "difficulty": "easy",
+     "question": "A scalene triangle has two sides of equal length.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                                      "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+     "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     }
+]
---
+++ Day17_quiz_game/question_model.py
@@ -0,0 +1,13 @@
+# create a question class with an __init__ with two attribute: text and answer
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+#
+# ques1 = Question("hello", True)
+# ques2 = Question("hel", False)
+#
+# print(ques2.text)
+#
+# print(ques1.answer)
