0c790531d6a43106be14971d581e32cb54c549a0
abicraf <70151728+abicraf@users.noreply.github.com>
2023-05-25 00:34:10 +0800

    Add files via upload

--- Day25_us-state-game/main.py
+++ Day25_us-state-game/main.py
@@ -19,10 +19,21 @@ correct_guess_count = 0
 guess_count = 1
 answer_state = 'Default'
 correct_guess_list = []
+missing_state = []
 
 while answer_state != None and guess_count < 50:
-    answer_state = screen.textinput(title=f"{correct_guess_count}/50 States Correct", prompt="What's another State's name?").title()
+    answer_state = screen.textinput(title=f"{correct_guess_count}/50 States Correct",
+                                    prompt="What's another State's name?").title()
     #print(answer_state, f"guess_count = {guess_count}")
+    if answer_state == 'Exit':
+        # save the missing states to a csv
+        for state in data_get:
+            if state not in correct_guess_list:
+                missing_state.append(state)
+        print(missing_state)
+        data_missing_state = pandas.DataFrame(missing_state)
+        data_missing_state.to_csv("missing_state.csv")
+        break
     for state_data in data_get:
         if answer_state == state_data['state']:
             state = turtle.Turtle()
@@ -36,5 +47,4 @@ while answer_state != None and guess_count < 50:
     #print(correct_guess_list)
     guess_count += 1
 
-
-screen.exitonclick()
+#screen.exitonclick()
