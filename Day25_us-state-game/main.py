bade4452c1d0eec9fed9e20f871f8aa33dc82082
abicraf <70151728+abicraf@users.noreply.github.com>
2023-05-24 23:45:43 +0800

    Add files via upload
    use file and dictionary to complete this project.

---
+++ Day25_us-state-game/main.py
@@ -0,0 +1,40 @@
+import turtle, csv, pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# read data from csv
+data = pandas.read_csv("50_states.csv")
+data_get = data.to_dict('records')
+# print(data_get)
+# print(data_get[0]['state'])
+# print(len(data_get))
+
+
+correct_guess_count = 0
+guess_count = 1
+answer_state = 'Default'
+correct_guess_list = []
+
+while answer_state != None and guess_count < 50:
+    answer_state = screen.textinput(title=f"{correct_guess_count}/50 States Correct", prompt="What's another State's name?").title()
+    #print(answer_state, f"guess_count = {guess_count}")
+    for state_data in data_get:
+        if answer_state == state_data['state']:
+            state = turtle.Turtle()
+            state.penup()
+            state.hideturtle()
+            state.goto(state_data['x'],state_data['y'])
+            state.write(f"{state_data['state']}")
+            if answer_state not in correct_guess_list:
+                correct_guess_list.append(answer_state)
+                correct_guess_count += 1
+    #print(correct_guess_list)
+    guess_count += 1
+
+
+screen.exitonclick()
