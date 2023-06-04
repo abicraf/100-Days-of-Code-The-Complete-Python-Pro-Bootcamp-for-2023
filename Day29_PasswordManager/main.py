1ef68461aef7e627cb08e3342c25b8420d8e4ff0
abicraf <70151728+abicraf@users.noreply.github.com>
2023-06-04 23:24:54 +0800

    Add files via upload
    Tkinter
    Canvas
    messagebox
    pyperclip: auto copy to the clipboard.

---
+++ Day29_PasswordManager/main.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def password_generator():
+    # clear the password in the entry box.
+    password_entry.delete(0, END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    #password_list = []
+
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+    letter_list = [random.choice(letters) for _ in range(nr_letters)]
+
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+    symbol_list = [random.choice(symbols) for _ in range(nr_symbols)]
+
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+    number_list = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = letter_list + symbol_list + number_list
+
+    random.shuffle(password_list)
+    # print(password_list)
+
+    # Use .join() to rephrase the code below
+    #
+    # password = ""
+    # for char in password_list:
+    #   password += char
+    password = "".join(password_list)
+
+    # Use pyperclip to auto copy the password to clipboard
+    pyperclip.copy(password)
+
+    # insert the new generated password in to the entry box.
+    password_entry.insert(0, password)
+    # print(f"Your password is: {password}")
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    # get the content in entries
+    web_content = web_entry.get()
+    email_content = email_entry.get()
+    password_content = password_entry.get()
+
+    # check if the content is correct before saving information to the file
+    if web_content == '' or password_content =='' or email_content =='':
+        messagebox.showinfo(title='Oops', message="Don't leave any field empty!")
+    else:
+        is_ok = messagebox.askokcancel(title="Please confirm",
+                                       message=f"Website:{web_content}\nEmail/User:{email_content}\n"
+                                               f"Password:{password_content}\n\n Is these correct?")
+        if is_ok:
+            # open a file and write the content
+            with open("password.txt", "a") as file:
+                # save the content in entries.
+                file.write(f"{web_content}  |  {email_content}  |  {password_content}\n")
+                # clear the content
+                web_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# Canvas
+img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=img)
+canvas.grid(row=0, column=1)
+
+# Label
+web_label = Label(text='Website:')
+web_label.grid(row=1, column=0)
+
+email_label = Label(text='Email/Username:')
+email_label.grid(row=2, column=0)
+
+password_label = Label(text='Password:')
+password_label.grid(row=3, column=0)
+
+# Entry
+web_entry = Entry(width=35)
+web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "kay@gmail.com")
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Button
+GenPassword_button = Button(text="Generate Password", width=15, command=password_generator)
+GenPassword_button.grid(row=3, column=2)
+
+Add_button = Button(text="Add", width=36, command=save)
+Add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
